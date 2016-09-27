b0ccaf5449768396f523fe5352c908e72ebd81a9
productnews <productnews@hotmail.com>
2016-09-27 20:05:01 +0800

    adding comment to MainActivity.java

--- app/src/main/java/com/apptivitylab/learn/todolist/MainActivity.java
+++ app/src/main/java/com/apptivitylab/learn/todolist/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // Github: testing purpose
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
